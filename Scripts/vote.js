e9458be9ebe2dc804470e103e39140551a725a45
Admin368 <58757588+Admin368@users.noreply.github.com>
2021-11-18 04:21:01 +0800

    + auto populate candidates from array

--- Scripts/vote.js
+++ Scripts/vote.js
@@ -1,40 +1,27 @@
-var isMenuOpen = false;
-
-const menuOpen = () => {
-    $('.menu-wrapper').css("height", "150px");
-    // $('.menu-wrapper').css("border", "1px solid white");
-    $('.menu-wrapper').css("opacity", "1");
-    isMenuOpen = true;
-}
-
-const menuClose = () => {
-    $('.menu-wrapper').css("height", "0px");
-    $('.menu-wrapper').css("border", "0px solid white");
-    $('.menu-wrapper').css("opacity", "0");
-
-    isMenuOpen = false;
-}
-
-const menuToggle = () => {
-    if (isMenuOpen == false) {
-        menuOpen();
-    } else {
-        menuClose();
-    }
-}
-
-$('.topBar-sandwich-wrapper').on('click', () => { menuToggle() });
+const imageFolder = '/Images/candidates/';
+const candidates = [
+    { id: 'candiate2', name: 'ALIX WONDER', position: 'President', image: imageFolder + 'candidate2.jpg' },
+    { id: 'candiate3', name: 'ALIX WONDER', position: 'President', image: imageFolder + 'candidate3.jpg' },
+    { id: 'candiate4', name: 'ALIX WONDER', position: 'President', image: imageFolder + 'candidate4.jpg' },
+]
 
 
-//LINKS
 
-$('.link').on('click', (e) => {
-    var link = e.target.id;
-    console.log(link);
-    menuClose();
-    $('.topBar').css('animation', 'fadeUp 2s forwards');
-    setTimeout(() => {
-        // alert('done');
-        window.location.href = link;
-    }, 1000)
-})
+candidates.map((candidate, index) => {
+    console.log(candidate);
+    const candidateTemplate = `
+        <li id='${candidate.id}' class='candidate-wrapper'>
+            <div class='candidate-image' style="background-image:url(${candidate.image});"></div>
+            <div class='candidate-name text'>${candidate.name}</div>
+            <div class='candidate-position text'>${candidate.position}</div>
+            <div class='candidate-options'>
+                <div class='button button-vote text'>CHOOSE</div>
+            </div>
+        </li>
+    `
+    $('.candidateList').append(candidateTemplate);
+
+});
+
+
+// $('.candidateList').ap

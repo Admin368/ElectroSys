466a3eaf872f87b9488f3525b27c016666b8940b
Admin368 <58757588+Admin368@users.noreply.github.com>
2021-11-17 19:38:08 +0800

    Initial commit

---
+++ Scripts/vote.js
@@ -0,0 +1,16 @@
+let width = window.innerWidth;
+let height = window.innerHeight;
+console.log('width:'+width+' height:'+height);
+
+
+// $( window ).resize(function() {
+//     // $( "#log" ).append( "<div>Handler for .resize() called.</div>" );
+//     // alert('resized');
+//     width = window.innerWidth;
+//     height = window.innerHeight;
+//     $('#bg').attr('width',width);
+//     $('#bg').attr('height',height);
+//     // $('#bg').attr('viewBox','0 0 '+width+' '+height);
+//     console.log('width:'+width+' height:'+height);
+// });
+

3a4dd689c0ce9d37fd2d0e376eac3335afc5dd86
Admin368 <58757588+Admin368@users.noreply.github.com>
2021-12-18 00:48:14 +0800

    register page logic and UI complete

---
+++ Scripts/register.js
@@ -0,0 +1,24 @@
+$(document).ready(() => {
+
+})
+
+// JQUERY AREA
+// $(".form-section-wrapper").submit(function(e) {
+//     e.preventDefault();
+// });
+
+function onFormRegisterSubmit() {
+    console.log('FORM LOGIN SUBMITTING');
+    var email = $('#input-email-value').val();
+    var domain = email.split('@')[1];
+    var form_notice = `
+        <div class='form-notice-wrapper'>
+            <p class=' text form-notice'>A link has been sent to your email</p>
+             <a/  href='http://${domain}'target="_blank" class='form-link text form-notice'>${email}<a/> 
+             <p class='text form-notice'> to reset your Password </p>
+        </div>
+    `;
+    $('#input-email').remove();
+    $('#button-register').remove();
+    $('.form-section-1').append(form_notice);
+}

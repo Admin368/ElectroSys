44431036fe9b33693799384ff453804a3fb74fbe
Admin368 <58757588+Admin368@users.noreply.github.com>
2021-12-18 00:50:20 +0800

    form adjustments + logic

--- Scripts/form.js
+++ Scripts/form.js
@@ -1,12 +1,14 @@
-$("#form-section-wrapper").submit(function(e){
+$(".form-section-wrapper").submit(function(e) {
     e.preventDefault();
+    var form_id = e.target.id;
     // console.log('submitted');
     // var childred = $("#form-section-1").children();
     // var childred = $("#form-section-1 > div").attr("id");
-    var childred = $("#form-section-1 > div");
+    var childred = $(".form-section-1 > div");
     // console.log(childred.length);
-    
-    for(let x=0;x<childred.length;x++){
+    var fieldsCount = childred.length;
+    var fieldsDone = 0;
+    for (let x = 0; x < fieldsCount; x++) {
         let id_ = childred[x].id;
         let id = $(`#${id_}`).data('input-id');
         let input_error_id = `input-${id}-error`;
@@ -15,30 +17,61 @@ $("#form-section-wrapper").submit(function(e){
         let error_div = `
             <div id='${input_error_id}' class='input-error'></div> 
         `;
-        if(value==""){// IF INPUT EMPTY
+        if (value == "") { // IF INPUT EMPTY
             input_error(id);
-        }else{// IF INPUT OK!
+        } else { // IF INPUT OK!
             input_ok(id);
+            console.log(form_id);
+            fieldsDone++;
+            if (fieldsDone == fieldsCount) {
+                if (form_id == 'form-register') { onFormRegisterSubmit() }
+                if (form_id == 'form-login') { onFormLoginSubmit() }
+            }
         }
-    }  
+    }
+});
+$('.input-item').hover((e) => {
+    // const candidate
+    var id = '#' + e.target.id;
+    var input_id = $(id).data('input-id');
+    var input_value_id = '#input-' + input_id + '-value';
+    $(input_value_id).css("animation", "expand 1s");
+    $(input_value_id).css("transition", "1s");
+    $(input_value_id).css("height", "20px");
+    // console.log(input_value_id);
+    // $(input_value_id).focus();
 });
 
-$('.input-value').on('input',function(e) {
+$('.input-item').blur((e) => {
+    // const candidate
+    var id = '#' + e.target.id;
+    var input_id = $(id).data('input-id');
+    var input_value_id = '#input-' + input_id + '-value';
+    // $(input_value_id).css("animation", "compress");
+    $(input_value_id).css("transition", "1s");
+    $(input_value_id).css("height", "0px");
+    $(input_value_id).focus();
+    console.log('blur');
+});
+
+$('.input-value').on('input', function(e) {
     let id = $(`#${e.target.id}`).data('input-id');
     let input_value_id = `input-${id}-value`;
     let value = $(`#${input_value_id}`).val();
-    if(value==""){// IF INPUT EMPTY
+    if (value == "") { // IF INPUT EMPTY
         input_error(id);
-    }else{// IF INPUT OK!
+    } else { // IF INPUT OK!
         input_ok(id);
-    }
-  });
+        //SUBMIT
 
-  function input_error(id){
+    }
+});
+
+function input_error(id) {
     let input_item_id = `input-${id}`;
     let input_value_id = `input-${id}-value`;
-    let input_error = $("#"+input_value_id).data('input-error');
-    if(input_error==false){
+    let input_error = $("#" + input_value_id).data('input-error');
+    if (input_error == false) {
         console.log(`input_empty:${input_value_id}`);
         let input_title_id = `input-${id}-title`;
         let input_error_id = `input-${id}-error`;
@@ -46,24 +79,25 @@ $('.input-value').on('input',function(e) {
             <div id='${input_error_id}' class='input-error'></div> 
         `;
         $(`#${input_item_id}`).append(error_div);
-        $(`#${input_item_id}`).css('border-bottom-color','#F05454');
-        $(`#${input_title_id}`).css('color','#F05454');
-        $(`#${input_value_id}`).data('input-error',true);
+        $(`#${input_item_id}`).css('border-bottom-color', '#F05454');
+        $(`#${input_title_id}`).css('color', '#F05454');
+        $(`#${input_value_id}`).data('input-error', true);
     }
-  }
-  function input_ok(id){
+}
+
+function input_ok(id) {
     let input_item_id = `input-${id}`;
     let input_value_id = `input-${id}-value`;
-    let input_error = $("#"+input_value_id).data('input-error');
-    if(input_error==true){
+    let input_error = $("#" + input_value_id).data('input-error');
+    if (input_error == true) {
         // console.log(input_value_id);
         console.log(`input_ok:${input_value_id}`);
         let input_title_id = `input-${id}-title`;
         let input_error_id = `input-${id}-error`;
         $(`#${input_item_id}`).find(".input-error").remove();
-        $(`#${input_item_id}`).css('border-bottom-color','white');
-        $(`#${input_title_id}`).css('color','white');
-        $(`#${input_value_id}`).data('input-error',false);
+        $(`#${input_item_id}`).css('border-bottom-color', 'white');
+        $(`#${input_title_id}`).css('color', 'white');
+        $(`#${input_value_id}`).data('input-error', false);
     }
-    
-  }
+
+}

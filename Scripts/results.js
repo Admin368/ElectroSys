ff5ac8076e5bb451df84ecc2ed136f4135316da1
Admin368 <58757588+Admin368@users.noreply.github.com>
2021-12-20 00:44:04 +0800

    results styling+logic Complete

--- Scripts/results.js
+++ Scripts/results.js
@@ -1,55 +1,89 @@
-$(document).ready(() => {
+var results = [];
+$(document).ready(async() => {
         // const ctx = document.getElementById('myChart');
         // const ctx = document.getElementById('myChart').getContext('2d');
         // const ctx = $('#myChart');
         // const ctx = 'myChart';
-        drawChat();
-        results_candidate_populate();
+        results = await generateResults();
+        drawChart('bar', results);
+        results_populate(results);
     })
     // JQUERY AREA
-function results_candidate_populate() {
+async function results_populate(results) {
+    //POPULATE TOTAL RESULTS
+    // var results = await generateResults();
+    const totalResults_Template = `
+        <li id='totalResults' class='result-row candidate-wrapper '>
+            <div class='result-div-equal'>
+                <div class='result-wrapper candidate-candidates-info'>
+                    <div class='result-title text'>TOTAL REGISTERED</div>
+                    <div class='result-value text'>${results.totalRegistered}</div>
+                </div>
+            </div>
+            <div class='result-div-equal'>
+                <div class='result-wrapper candidate-candidates-info'>
+                    <div class='result-title text'>TOTAL VOTED</div>
+                    <div class='result-value text'>${results.totalVoted}</div>
+                </div>
+            </div>
+            
+        </li>
+    `;
+    $('.resultList').append(totalResults_Template);
+
+
+    //POPULATE INDIVIDUAL RESULTS
     candidate_array.map((candidate, index) => {
-        // loadingHide();
         console.log(candidate);
-        const candidateTemplate = `
-        <li id='${candidate.id}' class='candidate-wrapper '>
-            <div class='candidate-results-info'>
+        let result_div_id = `result_${index}`
+        const candidate_Template = `
+        <li id='${candidate.id}' class='candidate-wrapper'>
                 <div class='candidate-image' style="background-image:url(${candidate.image});"></div>
                 <div class='candidate-name text'>${candidate.name}</div>
-                <div class='candidate-position text'>${candidate.position}</div>
-            </div>
-            <div class='candidate-results-graph'>
-
-            </div>
-        </li>
+                <div class='candidate-position text'>${candidate.position} <strong>#${index+1}</strong></div>
+                <div class='candidate-position text'>${candidate.votes}/${results.totalVoted} Votes</div>
+            </li>
     `
-        $('.candidateList').append(candidateTemplate);
+        $('.resultList').append(candidate_Template);
     });
 }
 
-function generateResults() {
+async function generateResults() {
     var results = {}
-    results.labels = ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange', '1', '2', '3'];
-    results.data = [12, 19, 3, 5, 2, 3, 1, 2, 3, 4, 5, 6, 7];
-    results.total = 0;
+    results.labels = [];
+    results.data = [];
+    results.totalRegistered = 0;
+    results.totalVoted = 0;
     candidate_array.map((candidate, index) => {
-        results.labels[index] = candidate_array[index].name;
         var max = 500;
         var min = 100;
-        results.data[index] = Math.random() * (max - min) + min;
+        //GIVE RESULTS OF EACH USER HERE
+        candidate_array[index].votes = parseInt(Math.random() * (max - min) + min);
+        results.totalVoted += parseInt(candidate_array[index].votes);
     });
+    candidate_array.sort((a, b) => b.votes - a.votes); //SORTS ARRAY
+    candidate_array.map((candidate, index) => {
+        candidate_array[index].percentage = ((candidate.votes / results.totalVoted) * 100).toFixed(2);
+        let perc = candidate_array[index].percentage;
+        results.labels.push(`${candidate.name}(${perc}%)`);
+        results.data.push(candidate.votes);
+    });
+    results.totalRegistered = results.totalVoted + 55;
     return results;
 }
 
-function chart(type, data) {
+function drawChart(type, data) {
     var results = data;
     const ctx = $('#myChart');
     const myChart = new Chart(ctx, {
+        // responsive: true,
+        scaleFontColor: "#FFFFFF",
         type: type,
         data: {
             labels: results.labels,
+            color: 'white',
             datasets: [{
-                label: '# of Votes',
+                label: `# of Votes`,
                 data: results.data,
                 backgroundColor: [
                     'rgba(255, 99, 132, 0.2)',
@@ -68,38 +102,53 @@ function chart(type, data) {
                     'rgba(255, 159, 64, 1)'
                 ],
                 borderWidth: 1,
-
             }]
         },
+
         options: {
+            indexAxis: 'x',
             scales: {
                 y: {
-                    beginAtZero: true
-                }
+                    beginAtZero: true,
+                    ticks: {
+                        color: "white"
+                    }
+                },
+                x: {
+                    beginAtZero: true,
+                    ticks: {
+                        color: "white"
+                    }
+                },
+
             },
             plugins: {
                 title: {
                     display: true,
-                    text: 'Custom Chart Title'
+                    text: 'RESULTS OF VOTES',
+                    color: 'white',
+                },
+                labels: {
+                    text: 'sdsd',
+                    color: 'white',
+
                 },
                 legend: {
                     labels: {
                         color: 'white',
                     },
-                    title: {
-                        color: 'white'
-                    }
                 }
 
             }
         }
     });
+    // Chart.defaults.global.defaultFontColor = "#fff";
 }
 
-function drawChat() {
-    var results = generateResults();
-    chart('bar', results);
-}
+// async function drawChat() {
+//     var results = await generateResults();
+//     chart('bar', results);
+// }
 // JQUERY AREA
 // $('.candidateList').on('click', '.button-vote', (e) => {
 //     // const candidate
